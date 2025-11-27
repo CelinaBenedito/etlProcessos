6e0369093e8cdaa6b5154a31f7e1056fa4fd6152
CelinaBenedito <celina.benedito@sptech.school>
2025-11-27 15:04:04 -0300

    Fix: Corrigindo lógica de processamento

--- src/main/java/school/sptech/Processo.java
+++ src/main/java/school/sptech/Processo.java
@@ -1,15 +1,42 @@
 package school.sptech;
 
 public class Processo {
-    private String nome;
+    private String nome,timestamp;
+    private Integer pid;
     private Double cpu, ram;
     private Long bytesLidos, bytesEscritos;
 
-    public Processo(String nome, Double cpu, Double ram, Long bytesLidos, Long bytesEscritos) {
+    public Processo(String timestamp, Integer pid, String nome, Double cpu, Double ram, Long bytesLidos, Long bytesEscritos) {
+        this.timestamp = timestamp;
+        this.pid = pid;
         this.nome = nome;
         this.cpu = cpu;
         this.ram = ram;
         this.bytesLidos = bytesLidos;
+        this.bytesEscritos = bytesEscritos;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public void setBytesLidos(Long bytesLidos) {
+        this.bytesLidos = bytesLidos;
+    }
+
+    public void setBytesEscritos(Long bytesEscritos) {
         this.bytesEscritos = bytesEscritos;
     }
 
@@ -41,15 +68,9 @@ public class Processo {
         return bytesLidos;
     }
 
-    public void setBytesLidos(long bytesLidos) {
-        this.bytesLidos = bytesLidos;
-    }
 
     public long getBytesEscritos() {
         return bytesEscritos;
     }
 
-    public void setBytesEscritos(long bytesEscritos) {
-        this.bytesEscritos = bytesEscritos;
-    }
 }
